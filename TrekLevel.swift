8428db28a7a3619854d0770687eb355ae8add6c8
Jason Kritikos <jkritikos@gmail.com>
2014-10-07 20:40:04 +0300

    trek models & trek selection skeleton

---
+++ TrekLevel.swift
@@ -0,0 +1,15 @@
+//
+//  TrekLevel.swift
+//  Spotrek
+//
+//  Created by Jason Kritikos on 10/7/14.
+//  Copyright (c) 2014 Boomar. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class TrekLevel {
+    var name:String!
+    var color:UIColor!
+}
