414a50345a4d2697f0c7fe8c7650b64a32174c33
Markos Hatzitaskos <mhatzitaskos@gmail.com>
2014-10-09 21:55:03 +0300

    Added all the buttons to the side menu

--- Spotrek/startScreen/HomeSideMenu.swift
+++ Spotrek/startScreen/HomeSideMenu.swift
@@ -67,6 +67,55 @@ class HomeSideMenu: UIView {
         profileButton.exclusiveTouch = true
         profileButton.tag = HomeSideMenuButtonType.Profile.toRaw()
         self.addSubview(profileButton)
+        
+        //Gallery button
+        imagePath = SharedEnvironment.Instance().resourcePath().stringByAppendingPathComponent("home/menu/gallery.png")
+        let galleryButtonImage = UIImage(contentsOfFile: imagePath)
+        galleryButton = UIButton.buttonWithType(UIButtonType.Custom) as UIButton
+        galleryButton.frame = CGRectMake(0, 0, galleryButtonImage.size.width, galleryButtonImage.size.height)
+        galleryButton.center = CGPointMake(self.frame.size.width/2, startY+dY)
+        galleryButton.setImage(galleryButtonImage, forState: UIControlState.Normal)
+        galleryButton.addTarget(self, action: "buttonWasPressed:", forControlEvents: UIControlEvents.TouchUpInside)
+        galleryButton.exclusiveTouch = true
+        galleryButton.tag = HomeSideMenuButtonType.Gallery.toRaw()
+        self.addSubview(galleryButton)
+        
+        //Settings button
+        imagePath = SharedEnvironment.Instance().resourcePath().stringByAppendingPathComponent("home/menu/settings.png")
+        let settingsButtonImage = UIImage(contentsOfFile: imagePath)
+        settingsButton = UIButton.buttonWithType(UIButtonType.Custom) as UIButton
+        settingsButton.frame = CGRectMake(0, 0, settingsButtonImage.size.width, settingsButtonImage.size.height)
+        settingsButton.center = CGPointMake(self.frame.size.width/2, startY+2*dY)
+        settingsButton.setImage(settingsButtonImage, forState: UIControlState.Normal)
+        settingsButton.addTarget(self, action: "buttonWasPressed:", forControlEvents: UIControlEvents.TouchUpInside)
+        settingsButton.exclusiveTouch = true
+        settingsButton.tag = HomeSideMenuButtonType.Settings.toRaw()
+        self.addSubview(settingsButton)
+        
+        //Store button
+        imagePath = SharedEnvironment.Instance().resourcePath().stringByAppendingPathComponent("home/menu/store.png")
+        let storeButtonImage = UIImage(contentsOfFile: imagePath)
+        storeButton = UIButton.buttonWithType(UIButtonType.Custom) as UIButton
+        storeButton.frame = CGRectMake(0, 0, storeButtonImage.size.width, storeButtonImage.size.height)
+        storeButton.center = CGPointMake(self.frame.size.width/2, startY+3*dY)
+        storeButton.setImage(storeButtonImage, forState: UIControlState.Normal)
+        storeButton.addTarget(self, action: "buttonWasPressed:", forControlEvents: UIControlEvents.TouchUpInside)
+        storeButton.exclusiveTouch = true
+        storeButton.tag = HomeSideMenuButtonType.Store.toRaw()
+        self.addSubview(storeButton)
+        
+        //Hints button
+        imagePath = SharedEnvironment.Instance().resourcePath().stringByAppendingPathComponent("home/menu/hints.png")
+        let hintsButtonImage = UIImage(contentsOfFile: imagePath)
+        hintsButton = UIButton.buttonWithType(UIButtonType.Custom) as UIButton
+        hintsButton.frame = CGRectMake(0, 0, hintsButtonImage.size.width, hintsButtonImage.size.height)
+        hintsButton.center = CGPointMake(self.frame.size.width/2, startY+4*dY)
+        hintsButton.setImage(hintsButtonImage, forState: UIControlState.Normal)
+        hintsButton.addTarget(self, action: "buttonWasPressed:", forControlEvents: UIControlEvents.TouchUpInside)
+        hintsButton.exclusiveTouch = true
+        hintsButton.tag = HomeSideMenuButtonType.Hints.toRaw()
+        self.addSubview(hintsButton)
+
     }
     
     func buttonWasPressed(sender: UIButton) {
